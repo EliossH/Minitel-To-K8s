62c9596f0f74471f4a94c9045105be1f2361eb71
EliossH <coheleachdamien@gmail.com>
2025-06-06 15:41:00 +0200

    removed pynitel

--- pynitel-master/VTXBuilder/Widget.py
+++
@@ -1,6 +0,0 @@
-class Widget:
-    def __init__(self, widget_list):
-        self.widget_list
-    
-    def render(self):
-        return NotImplementedError("render() function should be overwritten by child widget class")
